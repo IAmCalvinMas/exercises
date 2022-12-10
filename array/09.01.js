ad43c987099ec2ab82f9bd0bed10c04b9be3a05e
iamcalvinmas <calvinmasza@gmail.com>
2022-12-10 15:21:46 +0200

    updated dir

--- array/09.01.js
+++
@@ -1,7 +0,0 @@
-/*
- *
- */
-
-const result = input => Array.isArray(input);
-
-console.log(result([]));
--- basic/01.133.js
+++
@@ -1,8 +0,0 @@
-/*
- * desc: Write a JavaScript program to check whether a given fraction is proper or not. 
- * author: calvinmas@spacemilk
- */
-
-const result = frac => frac > 1 ? 'im': '' + 'proper fraction';
-
-console.log(result(3/4));
